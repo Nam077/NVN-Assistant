79cd75ce8063ef797bec5d1f090d09135e8891c3
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-14 21:39:57 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -125,6 +125,8 @@ function handlePostback(sender_psid, received_postback) {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    } else if (payload === 'GET_STARTED_PAYLOAD') {
+        response = { "text": "Chào bạn tôi là NVN" }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
