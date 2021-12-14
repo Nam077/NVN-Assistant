3eb6cc445bc496d0ad4f25573097616b41b9200f
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-14 21:54:41 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -120,13 +120,19 @@ function handlePostback(sender_psid, received_postback) {
     // Get the payload for the postback
     let payload = received_postback.payload;
 
-    // Set the response based on the postback payload
-    if (payload === 'yes') {
-        response = { "text": "Thanks!" }
-    } else if (payload === 'no') {
-        response = { "text": "Oops, try sending another image." }
-    } else if (payload === 'GET_STARTED_PAYLOAD') {
-        response = { "text": "Chào bạn tôi là NVN" }
+
+    switch (payload) {
+        case 'yes':
+            response = { "text": "Thanks!" }
+            break;
+        case 'no':
+            response = { "text": "Oops, try sending another image." }
+            break;
+        case 'GET_STARTED_PAYLOAD':
+            response = { "text": "Chào bạn tôi là NVN" }
+            break;
+        default:
+            response = { 'Xin lỗi tôi không hiểu' }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
