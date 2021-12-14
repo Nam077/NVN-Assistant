826101f31fe67c67750d2d0eaea225670cca38b1
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-14 22:35:47 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -4,7 +4,8 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 let handleGetStarted = (sender_psid) => {
     return new Promise(async(reslove, reject) => {
         try {
-            let response = { "text": "Chào bạn tôi là NVN" }
+            let username = await getUserName(sender_psid);
+            let response = { "text": `Chào ${username} tôi là NVN` }
             await callSendAPI(sender_psid, response);
             reslove('done');
         } catch (e) {
@@ -34,6 +35,26 @@ let callSendAPI = (sender_psid, response) => {
         } else {
             console.error("Unable to send message:" + err);
         }
+    });
+
+}
+
+let getUserName = async(sender_psid) => {
+    return new Promise((reslove, reject) => {
+        request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+            "method": "GET",
+        }, (err, res, body) => {
+            if (!err) {
+                body = JSON.parse(body);
+                let username = `${body.first_name} ${body.last_name}`;
+                reslove(username)
+            } else {
+                console.error("Unable to send message:" + err);
+                reject(err);
+            }
+        });
     });
 }
 module.exports = {
