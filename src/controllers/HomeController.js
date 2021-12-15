c20460a560ae409ed0bbfe91e4579c4a52c3ca25
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-15 11:46:49 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -75,7 +75,7 @@ async function handleMessage(sender_psid, received_message) {
     let response;
     // Checks if the message contains text
     if (received_message.text) {
-        let message = received_message.toLowerCase();
+        let message = received_message;
         console.log(message);
         if (message == 'nvn') {
             await chatbotService.sendMessage(sender_psid);
