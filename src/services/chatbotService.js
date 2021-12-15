9c4024e031f15840347c1e5fc8889ba10e06c47b
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-15 12:00:01 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -61,6 +61,18 @@ let sendMessage = (sender_psid) => {
     return new Promise(async(reslove, reject) => {
         try {
             let username = await getUserName(sender_psid);
+            let response2 = {
+                "attachment": {
+                    "type": "image",
+                    "payload": {
+
+                        "url": "https://scontent.fhan3-2.fna.fbcdn.net/v/t1.15752-9/s960x960/263466413_2909610869351584_6247793863845033518_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=ae9488&_nc_ohc=dCYspV0w4-kAX-JtLJ9&_nc_ht=scontent.fhan3-2.fna&oh=03_AVKjZqm-An2nkOfqduYD1YedN9NLRPo-FeH5iTpAoqas2A&oe=61E071D0",
+                        "is_reusable": true
+                    }
+                }
+
+            }
+            await callSendAPI(sender_psid, response2);
             let message = `Chào ${username}\nTôi đã nhận được yêu cầu từ bạn\nTên font: NVN Suýt nữa thì\nLink download: https://tinyurl.com/NVNVintAge\nVui lòng không phản hồi lại tin nhắn này\n#NVNFONT`
             let response = {
                 "attachment": {
@@ -77,18 +89,6 @@ let sendMessage = (sender_psid) => {
                 }
             }
             await callSendAPI(sender_psid, response);
-            let response2 = {
-                "attachment": {
-                    "type": "image",
-                    "payload": {
-
-                        "url": "https://scontent.fhan3-2.fna.fbcdn.net/v/t1.15752-9/s960x960/263466413_2909610869351584_6247793863845033518_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=ae9488&_nc_ohc=dCYspV0w4-kAX-JtLJ9&_nc_ht=scontent.fhan3-2.fna&oh=03_AVKjZqm-An2nkOfqduYD1YedN9NLRPo-FeH5iTpAoqas2A&oe=61E071D0",
-                        "is_reusable": true
-                    }
-                }
-
-            }
-            await callSendAPI(sender_psid, response2);
             reslove('done');
         } catch (e) {
             reject(e);
