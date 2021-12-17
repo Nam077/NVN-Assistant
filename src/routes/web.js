2a2043cf4b54752062e5595aab74b9b5bc55fae4
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-17 10:17:08 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -10,6 +10,7 @@ let initWebRoutes = (app) => {
     router.post('/webhook', homeController.postWebhook);
     router.get('/webhook', homeController.getWebhook);
     router.get("/excel", homeController.getGoogleSheet);
+    router.get("/crawler", homeController.getCrawler);
     return app.use('/', router);
 }
 
