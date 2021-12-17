6df853f9dd4ce99a0865adc31289e83011eb073d
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-17 13:49:05 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -131,7 +131,7 @@ async function handleMessage(sender_psid, received_message) {
             let response2 = { "text": 'Nếu bạn muốn lấy link nào thì nhắn tin tên một font trong list này\nHệ thống sẽ gửi cho bạn.' }
             await chatbotService.callSendAPI(sender_psid, response2);
         } else {
-            chatbotService.getGooleSearch(sender_psid, message);
+            await chatbotService.getGooleSearch(sender_psid, message);
         }
 
     } else if (received_message.attachments) {
