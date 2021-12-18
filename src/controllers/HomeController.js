895467b1533e3f7455f4ebb8982d978ac66e27e1
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-18 10:37:42 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -188,16 +188,19 @@ async function handlePostback(sender_psid, received_postback) {
             callSendAPI(sender_psid, response);
             break;
         case 'BOT_TUTORIAL':
-            response = { "text": "Vui lòng gửi tên font bạn cần tìm vào đây\nNếu không có bot sẽ không phản hồi. !" }
+            response = { "text": "Vui lòng gửi tên font bạn cần tìm vào đây\nNếu không có bot sẽ không phản hồi!" }
             await chatbotService.callSendAPI(sender_psid, response);
-            let response2 = { "text": "Nếu bạn muốn nhận hướng dẫn đầy đủ vui lòng gửi lại tin nhắn 'HDSD'." }
+            let response3 = chatbotService.getVideoTutorial();
+            await chatbotService.callSendAPI(sender_psid, response3);
+            let response2 = { "text": "Nếu bạn muốn nhận hướng dẫn đầy đủ vui lòng gửi lại tin nhắn 'HDSD'" }
             await chatbotService.callSendAPI(sender_psid, response2);
             break;
         case 'LIST_FONT':
             let msg = chatbotService.getFontSupport();
             response = { "text": msg }
             await chatbotService.callSendAPI(sender_psid, response);
-            break;
+            let response2 = { "text": 'Nếu bạn muốn lấy link nào thì nhắn tin tên một font trong list này\nHệ thống sẽ gửi cho bạn' }
+            await chatbotService.callSendAPI(sender_psid, response2);
         case 'PRICE_SERVICE':
             response = { "text": "Hiện tại bên mình nhận việt hóa với giá 50.000 đồng một font." }
             callSendAPI(sender_psid, response);
