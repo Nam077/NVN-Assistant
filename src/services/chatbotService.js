2ab3a63c87f3ffb62967e33404b4e3ef345b2f30
Nam077 <87227884+Nam077@users.noreply.github.com>
2021-12-20 14:46:14 +0700

    update wheather

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -255,8 +255,8 @@ let getGooleSearch = async(sender_psid, message) => {
                 return;
             }
             let msg = message.toLowerCase();
-            console.log(msg);
-            if (msg.indexOf('thời tiết') != -1 && !checkwheather.length > 0) {
+            msg = stripAccents(msg);
+            if (msg.indexOf('thoi tiet') != -1 && !checkwheather.length > 0) {
                 let response = { text: 'Nếu bạn muốn xem thời tiết\nThì nhắn tin phải có địa điểm\nVí dụ như thế này nè:' };
                 await callSendAPI(sender_psid, response);
                 let response2 = { text: 'Thời tiết tại Đà Nẵng' };
