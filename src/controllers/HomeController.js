f9fdf4e774843e6467394c729a2de472e3b25137
Nam077 <87227884+Nam077@users.noreply.github.com>
2022-01-01 11:14:48 +0700

    use npm

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -671,39 +671,8 @@ let googleTranslate = (text) => {
 }
 
 let test = async(req, res) => {
-    let fonts = 'http://localhost:8080/api/v1/fonts';
-    try {
-        var readData = fs.readFileSync('checkUpdate.txt', 'utf8');
-        console.log(readData.toString());
-    } catch (e) {
-        console.log('Error:', e.stack);
-    }
-    return new Promise((reslove, reject) => {
-        request.get(fonts, function(error, response, body) {
-            var fontObject = JSON.parse(body).font;
-            var dataObject = JSON.parse(body).data;
-            var listfontObject = JSON.parse(body).listfont;
-            var fontFile = fs.createWriteStream('font.json');
-            var dataFile = fs.createWriteStream('data.json');
-            var listFontFile = fs.createWriteStream('listfont.json');
-            try {
-                fs.writeFileSync('font.json', JSON.stringify(fontObject));
-                console.log("Lưu danh sách font thành công.");
-                fs.writeFileSync('data.json', JSON.stringify(dataObject));
-                console.log("Lưu danh sách font thành công.");
-                fs.writeFileSync('listfont.json', JSON.stringify(listfontObject));
-                console.log("Lưu danh sách font thành công.");
-                fs.writeFileSync("checkUpdate.txt", 'True');
-            } catch (error) {
-                console.error(err);
-            }
-            reslove('sdf');
-            return res.send(fontObject)
 
-        });
-
-
-    })
+    await updateData();
 }
 module.exports = {
     getHomePage: getHomePage,
