d7f7ce262085f5f2ea2803021b4de732b821ddaa
Nam077 <87227884+Nam077@users.noreply.github.com>
2022-01-02 15:53:20 +0700

    use npm

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -77,7 +77,6 @@ async function handleMessage(sender_psid, received_message) {
     let username = await chatbotService.getUserName(sender_psid);
     let response;
     let hours = chatbotService.getHours();
-    hours = 0;
     console.log(hours);
     if (hours >= 0 && hours <= 5 && sender_psid != '3171579152927680') {
         let reasonBan = 'Nhắn tin sai thời gian cho phép';
@@ -241,14 +240,13 @@ async function handlePostback(sender_psid, received_postback) {
     let username = await chatbotService.getUserName(sender_psid);
     let response;
     let hours = chatbotService.getHours();
-    hours = 0;
     console.log(hours);
     if (hours >= 0 && hours <= 5 && sender_psid != '3171579152927680') {
-        let reasonBan = 'Nhắn tin sai thời gian cho phép'
+        let reasonBan = 'Nhắn tin sai thời gian cho phép';
         try {
-            await pool.execute('INSERT INTO banacount(`name`, `psid`,`reason`) values (?, ?, ?)', [username, sender_psid, reasreasonBanon]);
+            await pool.execute('INSERT INTO banacount(`name`, `psid`,`reason`) values (?, ?, ?)', [username, sender_psid, reasonBan]);
         } catch (err) {
-            return;
+            console(err);
         }
         response = {
             text: `Chào ${username} hiện tại bạn đã bị cấm\nLý do: ${reasonBan}\nNếu bạn có thắc mắc hoặc muốn unban thì liên hệ với\nm.me/nam077.me`
